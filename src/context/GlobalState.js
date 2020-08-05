df206668367fbc9a046a73411222f7bddc398d93
SyedShahbazHussain <syedshahbaz.hussain@outlook.com>
2020-08-05 22:57:06 +0500

    working on context API

---
+++ src/context/GlobalState.js
@@ -0,0 +1,33 @@
+import React, { createContext, useReducer} from 'react'
+import AppReducer from "./AppReducer"
+
+//Initial state
+const initialState= {
+    tranactions:[
+  { id: 1, text: 'Flower', amount: -20 },
+  { id: 2, text: 'Salary', amount: 300 },
+  { id: 3, text: 'Book', amount: -10 },
+  { id: 4, text: 'Camera', amount: 150 }
+    ]
+}
+
+// Create Context 
+export const GlobalContext = createContext(initialState)
+
+//Provider component
+
+export const GlobalProvider=({childern})=>{
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    return(
+        <GlobalContext.Provider
+        value={{
+                tranactions: state.tranactions
+        }}
+        >
+
+            {childern}
+
+        </GlobalContext.Provider>
+    )
+}   
